6032f6e6dd018e9a4b4ff40042dd547a0c23892e
qldds <martin.nickelson@gmail.com>
2025-11-17 17:57:50 -0700

    Create Home.js

---
+++ client/public/src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div className="page">
+      <h2>Welcome to Outdoor Adventures!</h2>
+      <p>Your one-stop shop for camping, hiking, fishing, and hunting gear!</p>
+    </div>
+  );
+}
+
+export default Home;
